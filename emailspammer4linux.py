78dd82ad5b00b5681bdd8297363e0d8d58ec93fb
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-10 20:24:38 +0500

    emailspammer4linux.py
    Requirements:
    
     Python3

---
+++ emailspammer4linux.py
@@ -0,0 +1,55 @@
+
+import smtplib
+import os
+
+os.system('clear')
+
+
+print('''
+
+ ####### #######    #    #     #    ####### ######   #####   #####  
+    #    #         # #   ##   ##       #    #     # #     # #     # 
+    #    #        #   #  # # # #       #    #     #       #       # 
+    #    #####   #     # #  #  #       #    ######   #####   #####  
+    #    #       ####### #     #       #    #   #         #       # 
+    #    #       #     # #     #       #    #    #  #     # #     # 
+    #    ####### #     # #     #       #    #     #  #####   #####  
+                                                                    
+
+                                                                    
+''')
+
+
+print('''
+
+sign in with ur email account
+
+''')
+
+prot = input("Enter protocol (smtp.gmail.com for gmail): ")
+
+
+
+
+
+uname = input("Enter Your Email: ")
+upass = input("Enter Your password: ")
+rmail = input("Enter victim\'s Email: ")
+message = input("Enter the message you want to send: ")
+threads1 = input("Enter The amount of threads you want to send: ")
+s = smtplib.SMTP(prot, 587)
+
+s.starttls()
+
+s.login(uname, upass)
+
+msg = message
+
+for i in range(int(threads1)):
+    s.sendmail(uname, upass, msg)
+    print("spamming "+rmail)
+
+s.quit()
+
+
+Sent from ProtonMail Mobile
