4c600b1e2e23a86c2e86527db5bf0da30dcdce82
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2021-01-05 12:26:31 +0500

    Create emailspammer.py
    Requirements:
    
    Python3 
    
    Usage: python3 emailspammer.py

---
+++ emailspammer.py
@@ -0,0 +1,56 @@
+
+import smtplib
+import sys
+import os
+
+
+sys.ps1 = '\033[01;32m'
+print(sys.ps1)
+
+os.system('clear || cls')
+
+
+print('''
+
+ ####### ######   #####   #####  
+    #    #     # #     # #     # 
+    #    #     #       #       # 
+    #    ######   #####   #####  
+    #    #   #         #       # 
+    #    #    #  #     # #     # 
+    #    #     #  #####   #####  
+                                 
+                                                                    
+''')
+
+
+print('''
+
+sign in with ur email account
+
+''')
+
+prot = input("Enter protocol (smtp.gmail.com for gmail): ")
+
+
+
+
+
+uname = input("Enter Your Email: ")
+upass = input("Enter Your password: ")
+rmail = input("Enter victim\'s Email: ")
+message = input("Enter the message you want to send: ")
+threads1 = input("Enter The amount of threads you want to send: ")
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+s.starttls()
+
+s.login(uname, upass)
+
+msg = message
+
+for i in range(int(threads1)):
+   s.sendmail(upass, uname, msg)
+   print("spamming "+rmail)
+
+s.quit()
